b8a208390adc24b424b2ee5547ab1a141f396e34
edytaa3 <edytaadamczyk93@gmail.com>
2018-10-20 21:39:06 +0200

    Nieuposledzone kierowanie postacią

--- Czasodziej/Assets/PlayerController.cs
+++ Czasodziej/Assets/PlayerController.cs
@@ -12,9 +12,34 @@ public class PlayerController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-        float moveHorizonal = Input.GetAxis("Horizontal");
-        float moveVertical = Input.GetAxis("Vertical");
-        Vector2 movement = new Vector2(moveHorizonal, moveVertical);
-        rb2d.AddForce(movement*speed);
+        //float moveHorizonal = Input.GetAxis("Horizontal");
+        // float moveVertical = Input.GetAxis("Vertical");
+        //Vector2 movement = new Vector2(moveHorizonal, moveVertical);
+        //rb2d.AddForce(movement*speed);
+
+        Movement();
+    }
+
+    void Movement()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector2.left * speed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector2.right * speed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector2.up * speed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(Vector2.down * speed * Time.deltaTime);
+        }
     }
 }
