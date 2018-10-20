0b0cb277d01d04ccb56241d6c78cd4fc77efcf59
edytaa3 <edytaadamczyk93@gmail.com>
2018-10-20 20:18:29 +0200

    dodane upośledzone sterowanie

---
+++ Czasodziej/Assets/PlayerController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    public float speed;
+    private Rigidbody2D rb2d;
+	// Use this for initialization
+	void Start () {
+        rb2d = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        float moveHorizonal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector2 movement = new Vector2(moveHorizonal, moveVertical);
+        rb2d.AddForce(movement*speed);
+    }
+}
