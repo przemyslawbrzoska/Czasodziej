3900d5bbecd43d32f73645f35f6aeaf9dce10d85
Jan Kisielewicz <janekk97@users.noreply.github.com>
2018-10-21 11:49:30 +0200

    pojedynczy potwór
    Dodałem potwora, który kieruje się w stronę gracza. Gry gracz się przemieszcza potworowi zwiększa się szybkość, gdy gracz stoi, potwór się przemieszcza lecz zmniejsza mu się szybkość. (tempo zwiększania się szybkości jest mniejsze niż zwiększania; ustalono min. i maks. szybkość).
    Obecnie potwór nie omija przeszkód.

---
+++ Czasodziej/Assets/MonsterMovement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterMovement : MonoBehaviour
+{
+    public float speed;
+    public Vector2 cos;
+    private Rigidbody2D rb;
+    private bool lr = true;
+    PlayerController refScript;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        refScript = GameObject.FindWithTag("Player").GetComponent<PlayerController>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Movement();
+    }
+
+    void Movement()
+    {
+        if (!(Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.DownArrow)))
+        {
+            float step = speed * Time.deltaTime;
+            transform.position = Vector2.MoveTowards(transform.position, refScript.position, step);
+
+            if (speed > 5) speed -= 0.005f;
+        }
+
+        else if (speed <=30) speed += 0.02f;
+    }
+}
--- Czasodziej/Assets/PlayerController.cs
+++ Czasodziej/Assets/PlayerController.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour {
     public float speed;
     private Rigidbody2D rb2d;
+    public Vector2 position;
+
 	// Use this for initialization
 	void Start () {
         rb2d = GetComponent<Rigidbody2D>();
-	}
+    }
 	
 	// Update is called once per frame
 	void FixedUpdate () {
@@ -41,5 +43,7 @@ public class PlayerController : MonoBehaviour {
         {
             transform.Translate(Vector2.down * speed * Time.deltaTime);
         }
+
+        position = rb2d.transform.position;
     }
 }
