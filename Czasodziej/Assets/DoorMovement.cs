c5ead4f2607b0e4c9f12ed850b70fae8d2711a49
przemyslawbrzoska <pbrzoska4@gmail.com>
2018-10-21 12:09:06 +0200

    dodane drzwi, jeszcze nie do konca chodzą

---
+++ Czasodziej/Assets/DoorMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorMovement : MonoBehaviour {
+    public Vector2 increaseValues = new Vector2(0.1F, 0.1F);
+    public Vector3 targetPostion = new Vector3(16.75F, 8.31F, 0);
+    public float speed = 0.1F;
+
+    private Rigidbody2D rb2d;
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        transform.localPosition = Vector3.Lerp(transform.localPosition, targetPostion, Time.deltaTime * speed);
+            
+    }
+}
