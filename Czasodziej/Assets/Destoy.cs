17b08a13ed9de705697c6b83c172cf9ac91fdeda
edytaa3 <edytaadamczyk93@gmail.com>
2018-10-22 23:20:42 +0200

    Dodanie rynu ziemii

---
+++ Czasodziej/Assets/Destoy.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destoy : MonoBehaviour {
+
+    public float Time = 5f;
+    // Use this for initialization
+    void Start()
+    {
+        Destroy(gameObject, Time);
+    }
+}
