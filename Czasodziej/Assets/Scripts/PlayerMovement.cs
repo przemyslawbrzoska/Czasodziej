4619e00d4717e841c36eeefad0ad0ffe8d69985b
Jan Kisielewicz <janekk97@users.noreply.github.com>
2018-11-05 18:56:48 +0100

    Dodano pasek potworów
    Na razie jest brzydki i nie wiem jak się zachowa przy różnych prędkościach potworów ale to mogę sprawdzić dopiero jak będziemy mieli różne prędkości.

---
+++ Czasodziej/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+    Rigidbody2D rb;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey("a"))
+        {
+            Vector2 movement = new Vector2(-1,0) ;
+            rb.MovePosition(movement);
+        }
+        if (Input.GetKey("d"))
+        {
+            Vector2 movement = new Vector2(1, 0);
+            rb.MovePosition(movement);
+        }
+        if (Input.GetKey("w"))
+        {
+            Vector2 movement = new Vector2(0, 1);
+            rb.MovePosition(movement);
+        }
+        if (Input.GetKey("w"))
+        {
+            Vector2 movement = new Vector2(0, -1);
+            rb.MovePosition(movement);
+        }
+    }
+}
